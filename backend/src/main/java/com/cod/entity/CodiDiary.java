c4aa9f00d1dc74b63e29d2fdbb1b0b6e07169b72
co323co <co323co@gmail.com>
2021-10-26 16:58:02 +0900

    [BE] feat : Add Entity (#S05P31B202-127)

---
+++ backend/src/main/java/com/cod/entity/CodiDiary.java
@@ -0,0 +1,46 @@
+package com.cod.entity;
+
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.util.Date;
+
+import static javax.persistence.GenerationType.IDENTITY;
+
+@DynamicInsert
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter @Setter
+@Entity
+@Table(name = "codi_diary")
+public class CodiDiary {
+    @Id
+    @Column(name = "id", nullable = false, updatable = false)
+    @GeneratedValue(strategy = IDENTITY)
+    private int id;
+
+    @ManyToOne
+    @JoinColumn(name="user_id", nullable = false)
+    private User user;
+
+    @Column(name = "date", nullable = false)
+    private Date date;
+
+    @Column(name = "thumbnail", nullable = false, columnDefinition = "TEXT")
+    private String thumbnail;
+
+    @Column(name = "content", columnDefinition = "TEXT")
+    private String content;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private Date created_at;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at", nullable = false)
+    private Date updated_at;
+}
