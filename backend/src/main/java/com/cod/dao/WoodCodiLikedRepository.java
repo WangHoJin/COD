0804be376f5c3b5da46952ff11c35221945e1b05
Shimyeeun <yeeun7873@naver.com>
2021-11-02 10:34:31 +0900

    [BE] feat: Add woodCodiLiked api (#S05P31B202-151)

--- backend/src/main/java/com/cod/dao/WoodCodiLikedRepository.java
+++ backend/src/main/java/com/cod/dao/WoodCodiLikedRepository.java
@@ -1,13 +1,11 @@
 package com.cod.dao;
 
-import com.cod.entity.Codi;
-import com.cod.entity.CodiLiked;
-import com.cod.entity.WoodCodi;
-import com.cod.entity.WoodCodiLiked;
+import com.cod.entity.*;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface WoodCodiLikedRepository extends JpaRepository<WoodCodiLiked, Integer> {
     int countByWoodCodi(WoodCodi woodCodi);
+    WoodCodiLiked findByWoodCodiAndUser(WoodCodi woodCodi, User user);
 }
