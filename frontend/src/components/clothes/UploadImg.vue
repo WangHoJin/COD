57dee1618f9813d4eb676c9ff8d0fc6aacfbf2f4
co323co <co323co@gmail.com>
2021-11-18 04:12:46 +0900

    [BE] feat : Update UploadImg.vue

--- frontend/src/components/clothes/UploadImg.vue
+++ frontend/src/components/clothes/UploadImg.vue
@@ -27,35 +27,52 @@
     </v-row>
     <v-file-input
       class="upload"
-      label="File input"
+      v-model="file"
+      hide-input
       filled
       prepend-icon="mdi-camera"
-      @change="change()"
+      @change="change"
     ></v-file-input>
   </v-container>
 </template>
 <script>
+import axios from "@/utils/axios";
 export default {
-  data: function () {
-    return {};
+  data() {
+    return {
+      file : null
+    }
   },
   methods: {
     // 이미지 업로드
-    change() {
-      ext = $(this).val().split(".").pop().toLowerCase(); //확장자
-      //배열에 추출한 확장자가 존재하는지 체크
-      if ($.inArray(ext, ["gif", "png", "jpg", "jpeg"]) == -1) {
-        resetFormElement($(this)); //폼 초기화
+    change(file) {
+      console.log(file);
+      let ext = file.type.toLowerCase(); //확장자
+      // //배열에 추출한 확장자가 존재하는지 체크
+      if (!(ext == "image/gif" || ext == "image/png" || ext == "image/jpg" || ext == "image/jpeg")) {
+        // resetFormElement($(this)); //폼 초기화
+        this.file==null;
         window.alert("이미지 파일이 아닙니다! (gif, png, jpg, jpeg 만 업로드 가능)");
       } else {
-        file = $("#img").prop("files")[0];
-        blobURL = window.URL.createObjectURL(file);
-        $("#image_preview img").attr("src", blobURL);
-        $("#image_preview").slideDown(); //업로드한 이미지 미리보기
-        $(this).slideUp(); //파일 양식 감춤
+        // file = $("#img").prop("files")[0];
+        // blobURL = window.URL.createObjectURL(file);
+        // $("#image_preview img").attr("src", blobURL);
+        // $("#image_preview").slideDown(); //업로드한 이미지 미리보기
+        // $(this).slideUp(); //파일 양식 감춤
+        let accessToken = this.$store.state.auth.accessToken;
+        var fd = new FormData();
+        fd.append('images', this.file);
+        axios.post('/images', fd, 
+            {
+              headers: {
+                'X-ACCESS-TOKEN': accessToken,
+                'Content-Type': 'multipart/form-data'
+              }
+            }
+        ).then(res=>{console.log(res.data)}).catch(error=>{console.log(error.response.data.message)}); 
       }
-    },
-  },
+   },
+ },
 };
 </script>
 <style scoped>
@@ -74,4 +91,5 @@ export default {
   align-items: center;
   text-align: center;
 }
+
 </style>
